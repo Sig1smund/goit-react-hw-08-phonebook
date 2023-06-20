55eb5bd48adc255a4ae5e06af0df3afe79d6fcbc
Sig1smund <87072894+Sig1smund@users.noreply.github.com>
2023-06-20 23:49:57 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import { createRoot } from 'react-dom/client';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
-import { BrowserRouter as Router } from 'react-router-dom';
+// import { BrowserRouter as Router } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import Spinner from './utils/Spinner';
@@ -11,14 +12,18 @@ import './index.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
+const router = createHashRouter([
+	{
+		path: '/*',
+		element: <App />,
+	},
+]);
 
 root.render(
 	<React.StrictMode>
 		<Provider store={store}>
 			<PersistGate loading={<Spinner />} persistor={persistor}>
-				<Router basename="/goit-react-hw-08-phonebook/">
-					<App />
-				</Router>
+				<RouterProvider router={router} />
 			</PersistGate>
 		</Provider>
 	</React.StrictMode>
